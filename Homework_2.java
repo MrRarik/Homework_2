c2160730d2daf62669e201d7b1aba73715daddf6
MrRarik <49559820+MrRarik@users.noreply.github.com>
2019-06-03 13:37:42 +0300

    Update Homework_2.java

--- Homework_2.java
+++ Homework_2.java
@@ -14,6 +14,8 @@ public class Homework_2 {
         fillDiagonal();
         checkMaxAndMin();
       }
+    //1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    //Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
         public static void invertArray () {
             int[] arr = {0, 1, 1, 0, 1, 0};
             for (int i = 0; i < arr.length; i++) {
@@ -25,6 +27,7 @@ public class Homework_2 {
             }
             System.out.println(Arrays.toString(arr));
         }
+    //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
         public static void fillArray () {
             int[] arr = new int[8];
             for (int i = 0, j = 0; i < arr.length; i++, j = j + 3) {
@@ -32,6 +35,7 @@ public class Homework_2 {
             }
             System.out.println(Arrays.toString(arr));
         }
+    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
         public static void changeArray() {
          int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
          for (int i = 0; i<arr.length; i++) {
@@ -41,6 +45,8 @@ public class Homework_2 {
          }
          System.out.println(Arrays.toString(arr));
       }
+    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+    // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
     public static void fillDiagonal() {
         int n = 4;
         int k = n - 2;
@@ -55,6 +61,7 @@ public class Homework_2 {
             System.out.println();
           }
         }
+    //5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
         public static void checkMaxAndMin(){
         int[] arr = {5, 7, 21, 1, 10, 6, 12, 0};
         int max = arr[0];
